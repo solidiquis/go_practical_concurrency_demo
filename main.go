b1510d381cbaad03a5560923e88661e6482bacce
Benjamin Nguyen <benjamin.van.nguyen@gmail.com>
2020-12-17 01:30:45 -0800

    some edits

--- main.go
+++ main.go
@@ -13,21 +13,21 @@ The files (located in the assets directory):
 - the_shadow_over_innsmouth.txt: 155kb
 
 The traditional approach:
-- Read text file, format data, and write to new file one by one.
+- Read text file, format data, and write to new file one-by-one.
 
 Pros to the traditional approach:
-- Synchronous programming is generally an easier mental model
-  which makes writing the program easier for most.
-- Generally safer as you won't run into data race issues, that is
+- Synchronous programming is generally an easier and more familiar
+  mental model which makes writing the program easier for most.
+- Generally safer as you won't run into data race issues; that is,
   you'll never have two or more independent processes try to access
-  the same piece of data when at least one of the access is a write.
+  the same piece of data when at least one of the accesses is a write.
 
 Cons to the traditional approach:
 - Because things are being done one-by-one, larger files
   can cause bottlenecks. Imagine if a chef decided to cook your main
-  dish first, THEN cooked your appetizer, because for some reason the
-  waiter listed the main as the first item in the list; now your appetizer
-  is going to awkwardly come out after you main dish.
+  dish first, THEN your appetizer, because for some reason the
+  waiter listed the main first in the list of food item; now your
+  appetizer is going to awkwardly come out after you main dish.
 
 Concurrency approach:
 - Spin up a goroutine for each file to handle the reading and
@@ -37,21 +37,20 @@ Concurrency approach:
 
 Pros to the concurrency approach:
 - No bottlenecks as each file is being worked on in individual goroutines,
-  which means that, in the context of this program, the smallest file will
+  which means that - in the context of this program - the smallest file will
   finish first, even if it's the last to be placed in a goroutine. The chef
-  basically now has an assistant who works on the appetizer while the chef
-  works concurrently on the main (which takes longer), meaning you'll get
-  your appetizer first.
+  can now work on the main while their assistant can concurrently work on the
+  appetizer, which is quicker to finish and send out to your table.
 - Overall performance.
 
 Cons to the concurrency approach:
-- Unfamiliar mental model for programmers, generally, which can make it
+- Generally an unfamiliar mental model for most programmers, which can make it
   challenging to implement.
 - You may unknowingly create data races.
-- Goroutine leaks can happen, in which a goroutine that you thought terminated,
-  lives for the lifetime of the programm which holds memory hostage; the
-  garbage collector also won't clean up goroutines, making this memory
-  leak especially dangerous.
+- Goroutine leaks can happen, which means that a goroutine that you thought
+  had terminated, lives for the lifetime of the program which holds memory
+  hostage. To make matters worse, the garbage collector will not clean up
+  goroutines, making this type of memory leak especially dangerous.
 
 The following code demonstrates the concurrency approach:
 */
@@ -81,7 +80,7 @@ func formatText(filename string, hplCh chan<- []byte) {
 	must(err)
 	data = re.ReplaceAll(data, []byte("\n"))
 
-	// Remove tabs and/or spaces
+	// Remove unwanted tabs and/or spaces
 	re, err = regexp.Compile(`^\s+`)
 	must(err)
 	data = re.ReplaceAll(data, []byte(""))
@@ -115,7 +114,7 @@ func formatText(filename string, hplCh chan<- []byte) {
 }
 
 func writeFormattedText(data []byte) {
-	fmt.Printf("Writing file of length: %v\n", len(data))
+	fmt.Printf("Writing file of length: %d\n", len(data))
 	file := fmt.Sprintf("tmp/%d.txt", len(data))
 	err := ioutil.WriteFile(file, data, 0644)
 	must(err)
